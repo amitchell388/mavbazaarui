31087b6de4d60edc8621f885fa7799ed12ee0d3b
Aaron Mitchell <amitchell388@gmail.com>
2026-02-04 18:53:56 -0600

    add initial files

---
+++ src/Pages/Welcome.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+const Welcome = () => {
+    const navigate = useNavigate();
+    return (
+        <>
+            <h1 style={{ color: "green" }}>
+                A Computer Science portal for geeks.
+            </h1>
+            <button onClick={() => navigate(-1)}>
+                Go Back Home
+            </button>
+        </>
+    );
+};
+export default Welcome;
