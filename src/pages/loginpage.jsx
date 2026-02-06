d3b881054f0faecece4f7ab7921f843395ac63d2
Kevin M <kevinlookup@gmail.com>
2026-02-06 10:57:18 -0600

    made login

--- src/pages/loginpage.jsx
+++ src/pages/loginpage.jsx
@@ -1,5 +1,6 @@
 
 import { useNavigate } from 'react-router-dom'
+import '../css/login.css'
 
 
 
@@ -8,15 +9,31 @@ function LoginPage() {
 
     return (
         <>
-            <h1>
-                Login Page
-            </h1>
+        <div id='main_div'>
+            <div>
+                <span className='big-words'>
+                    <span style={{ color: '#1173d4' }} >
+                        MAV
+                    </span>
+                    <span style={{ color: '#f97a1f' }} >
+                        BAZZAR
+                    </span>
+                </span>
+            </div>
+            <div>
+                <h3 style={{ color: 'black' }} >A student marketplace, built for Mavericks.</h3>
+            </div>
+            <div className='login-buttons'>
 
-            <button onClick={() => navigate('/home') }>
-                <h1>
-                    go home
-                </h1>
-            </button>
+                <button className='login-button-but'  style={{ backgroundColor: '#147be4' }} onClick={() => navigate("/home")}>
+                    Sign in with UTA Email
+                </button>
+                <button className='login-button-but' style={{ backgroundColor: '#f9751e' }} onClick={() => navigate("/home")}>
+                    Enter The Bazzar
+                </button>
+
+            </div>
+        </div>
 
         </>
  
